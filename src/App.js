363c8895ce311102fd8bff6e2acb6173f9b9699f
Federico Knüssel <fknussel@gmail.com>
2017-02-20 14:23:05 -0300

    Update styling and widget config

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import styles from './App.css';
 import logo from './logo.svg';
 
 const App = () => (
-  <section className={styles.app}>
-    <div className={styles.header}>
+  <section className={styles.wrapper}>
+    <header className={styles.header}>
       <img src={logo} className={styles.logo} alt="logo" />
-      <h2>Wordpress React Widget</h2>
-    </div>
-    <p className={styles.intro}>
+      <h2 className={styles.title}>Wordpress React Widget</h2>
+    </header>
+
+    <p className={styles.blurb}>
       Bacon ipsum dolor amet drumstick turducken biltong alcatra turkey. Turkey burgdoggen salami sirloin biltong. Tongue jowl ball tip ham meatball tenderloin tail kielbasa capicola short ribs leberkas pancetta shoulder ham hock. Bresaola kevin short loin ham shoulder venison.
     </p>
   </section>
--- wordpress-widget-example.php
+++ wordpress-widget-example.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Wordpress-Widget-Example
+Plugin Name: React Widget Example
 Description: Wordpress Widget powered by ReactJS
 Version:     0.1.0
 Author:      Federico Knüssel
@@ -12,8 +12,8 @@ function wp_widget_example() {
 }
 
 function include_react_files() {
-    wp_enqueue_style( 'prefix-style', plugins_url('css/main.9a0fe4f1.css', __FILE__) );
-    wp_enqueue_script( 'plugin-scripts', plugins_url('js/main.fdc4e6ad.js', __FILE__), array(), '0.1.0', true );
+    wp_enqueue_style( 'prefix-style', plugins_url('bundle.css', __FILE__) );
+    wp_enqueue_script( 'plugin-scripts', plugins_url('bundle.js', __FILE__), array(), '0.1.0', true );
 }
 
 add_action( 'wp_enqueue_scripts', 'include_react_files' );
